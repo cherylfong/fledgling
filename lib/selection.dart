31a91d49cba0f23906957e8ed8c033ba471fc5c0
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-09-27 11:18:02 -0700

    create selection class

---
+++ lib/selection.dart
@@ -0,0 +1,12 @@
+// To imports tidy, follow the ordering guidelines at
+// https://www.dartlang.org/guides/language/effective-dart/style#ordering
+import 'package:flutter/material.dart';
+
+class Selection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
